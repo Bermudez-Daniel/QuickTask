76dcdb08b35f438883f555a20812e18c1168eb6a
Daniel Bermudez <741757@student.dusd.net>
2024-08-22 15:24:26 -0700

    Update
    Added movement but colliders need some work.

--- My project/Assets/scripts/Movement.cs
+++ My project/Assets/scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -10,9 +11,23 @@ public class Movement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    public float verticalInput;
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+       
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+   
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          transform.Translate(Vector3.up * Time.deltaTime * 1000);
+        }
     }
+
 }
